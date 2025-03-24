dc150e509728053f019a1297e04edbf4af97bd3b
identityofsine <identityofsine@gmail.com>
2025-03-23 20:05:27 -0400

    adding about me

---
+++ src/client/components/AboutMe.tsx
@@ -0,0 +1,16 @@
+import '@/client/styles/aboutme.scss';
+
+export default function AboutMe() {
+	return (
+		<section className="about-me">
+			<div className="about-me__content">
+				<h1 className="about-me__header">
+					About Me
+				</h1>
+				<p className="about-me__paragraph">
+					Hello! I'm Kevin, a full stack engineer from the United States. I have a passion for building and designing web applications. I'm a firm believer in the power of technology to solve problems and make life easier. I'm always looking for new challenges and opportunities to learn and grow. When I'm not coding, you can find me hiking, reading, or playing video games.
+				</p>
+			</div>
+		</section>
+	);
+}
